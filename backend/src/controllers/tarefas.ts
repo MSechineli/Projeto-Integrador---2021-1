fff909432f64963e68fbf03df1e1e960aa3a5241
MSechineli <matheussechineli@gmail.com>
2021-07-14 16:44:30 -0300

    Criar e Ler

--- backend/src/controllers/tarefas.ts
+++ backend/src/controllers/tarefas.ts
@@ -3,14 +3,20 @@ import { connection } from '../database/connection';
 
 class Tarefas {
     async create(req: Request, res: Response){
-        // const {nome, descricao, data} = req.body;
-        // console.log(nome, descricao, data);
-        // const ret = connection('Tarefas').insert({
-        //     nome, 
-        //     descricao,
-        //     data
-        // })
-        return res.send({"TESTE": "FOI"})
+        const {nome, descricao, data} = req.body;
+        const teste = Date.UTC(2021, 6, 20);
+        const ret = await connection('Tarefas').insert({
+            nome, 
+            descricao,
+            data: teste
+        })
+        console.log(ret)
+        return res.send({
+            "nome": nome, 
+            "data": data, 
+            "descricao": descricao,
+            "response": ret
+        });
     }
     async read(request: Request, response: Response){
         const dados = await connection('Tarefas');
